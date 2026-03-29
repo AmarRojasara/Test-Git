2ad82c3adfe865599dfc332803e26e76d57c0e4f
AmarRojasara <rojasaraamar5@gmail.com>
2026-03-29 12:08:04 +0530

    First commit

---
+++ test.py
@@ -0,0 +1,17 @@
+a = [1,2,3,4,5,1,2,3]
+
+def count(numbers):
+    frequency={}
+    for number in numbers:
+        if number in frequency:
+            frequency[number] += 1
+        else:
+            frequency[number] = 1
+    
+    return frequency
+
+A = count(a)
+print(A)   
+
+B = set(a)
+print(B)
